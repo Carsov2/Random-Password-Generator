7aece341ffaac9e347148b4dbf862c4a3f922f01
Carsov2 <carsonkennedy2018@gmail.com>
2022-10-31 17:58:51 -0500

    second push

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,28 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+
+var userInput = window.prompt("Hey There")
+
+var passwordLength = parseInt(userInput)
+
+if (isNaN(passwordLength)) {
+  window.alert("That's not a number!")
+  return
+} else {
+  window.alert("That is a valid number!")
+}
+
+if (passwordLength < 8 || passwordLength > 128) {
+  window.alert("password length must be between 8 and 128 characters")
+  return 
+}
+
+}
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
