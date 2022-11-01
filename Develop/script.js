e53160618c69da6bf82f0ce8f8ba62d3a1b2c8d9
Carsov2 <carsonkennedy2018@gmail.com>
2022-10-31 23:56:30 -0500

    almost finished...

--- Develop/script.js
+++ Develop/script.js
@@ -4,9 +4,22 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function randomInt(min, max) {
+  if (!max) {
+    max = min
+    min = 0
+  }
+  var rand = Math.random()
+  return Math.floor(min*(1 - rand) + rand*max)
+}
+
+function getRandomItem(list) {
+  return list[randomInt(list.length)]
+}
+
 function generatePassword() {
 
-var userInput = window.prompt("Hey There")
+var userInput = window.prompt("How long would you like your password to be?")
 
 var passwordLength = parseInt(userInput)
 
@@ -22,6 +35,52 @@ if (passwordLength < 8 || passwordLength > 128) {
   return 
 }
 
+var userWantsNumbers = window.confirm("Would you like to include numbers in your password?")
+
+var userWantsNumbers = window.confirm("Would you like to include numbers in your password?")
+var userWantsSymbols = window.confirm("Would you like to include symbols in your paassword?")
+var userWantsLowercase = window.confirm("Would you like to include lowercase letters in your password?")
+var userWantsUppercase = window.confirm("Would you like to include uppercase letters in your password?")
+
+var numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var symbolList = ["!", "@", "#", "$", "%", "^", "&", "*"]
+var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var uppercaseList = []
+
+var optionsCart = []
+
+for (var i = 0; i < lowercaseList.length; i++) {
+uppercaseList[i] = lowercaseList[i].toUpperCase()
+
+}
+
+if (userWantsNumbers === true) {
+  optionsCart.push(numberList)
+}
+
+if (userWantsSymbols === true) {
+  optionsCart.push(symbolList)
+}
+if (userWantsLowercase === true) {
+  optionsCart.push(lowercaseList)
+}
+if (userWantsUppercase === true) {
+  optionsCart.push(uppercaseList)
+}
+
+if (optionsCart.length === 0) {
+  optionsCart.push(lowercaseList)
+}
+
+var generatedPassword = ""
+
+for (var i = 0; i < passwordLength; i++) {
+  var randomList = getRandomItem(optionsCart)
+  var randomChar = getRandomItem(randomList)
+  generatedPassword += randomChar
+} 
+
+return generatePassword
 }
 
 
